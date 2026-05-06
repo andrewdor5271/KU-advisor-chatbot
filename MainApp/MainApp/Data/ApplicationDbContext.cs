0dde6475c529067a5e1e2e787438e350f9a6d5b3
andrewdor5271 <firefighter52715271@gmail.com>
2026-05-06 15:05:08 +0200

    Initial commit; ASP.NET Core project initialized

---
+++ MainApp/MainApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MainApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
