870aca6f14863e410a167152aff648eec6c65b39
andrewdor5271 <firefighter52715271@gmail.com>
2026-07-04 17:45:09 +0200

    Conversation buttons implemented as partials

---
+++ MainApp/MainApp/Pages/Elements/GlobalPartials/ConversationButtonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MainApp.Pages.Elements.GlobalPartials
+{
+    public class ConversationButtonModel
+    {
+        public String Title { get; set; } = Consts.DUMMY_CONVO_DEFAULT_TITLE;
+        public required String Id { get; set; }
+        public required  String Url { get; set; }
+    }
+}
